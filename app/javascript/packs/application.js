e31d867469d563dc741b24d5af797d65cbfc8ee2
John Marsden <john.marsden24@gmail.com>
2020-04-10 17:25:53 +0100

    added image slideshow

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,11 @@
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+document.addEventListener('turbolinks:load', () => {
+});
+
+
