a0f2c9ceec4f8134d4dcfb705982e4a5d80821f2
John Marsden <john.marsden24@gmail.com>
2020-04-25 16:20:01 +0100

    added Meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV["www.johnmarsden.dev"] || "localhost:3000" }
+  end
+
 end
