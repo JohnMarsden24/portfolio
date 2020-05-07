7d7f8c9a9e281ca22fafdb3c56cbf68c15237c3c
John Marsden <john.marsden24@gmail.com>
2020-05-07 19:57:13 +0100

    pushing to heroku

--- db/migrate/20200409172354_create_projects.rb
+++ db/migrate/20200409172354_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :color
+      t.string :url
       t.string :github
       t.text :technologies, array: true, default: []
       t.text :description
