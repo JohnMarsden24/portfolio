5c9fcff1fc3ef7e06fbed59b334388b3955431f5
John Marsden <john.marsden24@gmail.com>
2020-06-04 11:57:46 +0100

    changed settings for mail form in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-config.action_mailer.default_url_options = { :host => 'john-marsden-portfolio.herokuapp.com' }
+# config.action_mailer.default_url_options = { :host => 'john-marsden-portfolio.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
@@ -119,7 +119,7 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'john-marsden-portfolio.herokuapp.com',
+  domain:               'gmail.com',
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
