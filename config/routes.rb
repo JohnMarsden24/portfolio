e5a09f7532c03727fae044cedc6614bbfe76eeed
John Marsden <john.marsden24@gmail.com>
2021-04-12 12:07:22 +0100

    removed download link

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get "/about", to: "pages#about"
-  get 'download_pdf', to: "pages#download_pdf"
-  get "/contact", to: "contacts#new", as: 'contact'
   get "/admin_home", to: "admin#home"
   resources :projects, param: :slug
-  resources :contacts, only: [:create]
 
   devise_for :users, skip: [:sessions, :registrations, :passwords]
 
